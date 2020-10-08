12a91edb1df9c64446fad8dc6c6201a51840b83a
Administrator <Administrator@L707-02.saigontech.edu.vn>
2020-10-08 17:17:39 +0700

    JUnit Test

--- ShoeProjectDemo/src/test/java/com/project/ShoeProjectDemoApplicationTests.java
+++ ShoeProjectDemo/src/test/java/com/project/ShoeProjectDemoApplicationTests.java
@@ -1,13 +1,45 @@
 package com.project;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import com.project.dtos.ProductReturnDto;
+import com.project.model.Product;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ShoeProjectDemoApplicationTests {
+	@LocalServerPort
+	private int port;
 
-	@Test
-	void contextLoads() {
+	RestTemplate restTemplate = new RestTemplate();
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
 	}
 
+	@Test
+	public void testGetAllProducts() {
+		ResponseEntity<Product[]> responseEntity = restTemplate.getForEntity(getRootUrl() + "/listProducts", Product[].class);
+		List<Product> products = Arrays.asList(responseEntity.getBody());
+		assertNotNull(products);
+	}
+	
+	@Test
+	public void testGetAllDetail() {
+		ResponseEntity<ProductReturnDto[]> responseEntity = restTemplate.getForEntity(getRootUrl() + "/listDetails", ProductReturnDto[].class);
+		List<ProductReturnDto> productDetails = Arrays.asList(responseEntity.getBody());
+		assertNotNull(productDetails);
+	}
+	
 }
